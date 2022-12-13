cd722f071db5f914e6e434e33bfc52ed08073889
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-12-13 17:13:42 +0100

    get & set di product

--- Models/Product.php
+++ Models/Product.php
@@ -5,13 +5,15 @@ class Product {
     protected String $image;
     protected Float $price;
     public Category $category;
+    private bool $avaliable;
 
-    function __construct(String $_title, String $_image, Float $_price, Category $_category)
+    function __construct(String $_title, String $_image = null, Float $_price, Category $_category)
     {
         $this->setTitle($_title);
         $this->setImage($_image);
         $this->setPrice($_price);
         $this->category = $_category;
+        $this->setAvaliable();
     }
     public function getTitle()
     {
@@ -19,7 +21,11 @@ class Product {
     }
     public function setTitle($title)
     {
-        $this->title = $title;
+        if(strlen($title)){
+            $this->title = $title;
+        }else{
+            $this->title = null;
+        }
         return $this;
     }
     public function getImage()
@@ -28,7 +34,11 @@ class Product {
     }
     public function setImage($image)
     {
-        $this->image = $image;
+        if(strlen($image)){
+            $this->image = $image;
+        }else{
+            $this->image = 'default.jpg';
+        }
         return $this;
     }
     public function getPrice()
@@ -37,7 +47,24 @@ class Product {
     }
     public function setPrice($price)
     {
-        $this->price = $price;
+        if($price > 0){
+            $this->price = $price;
+        }else{
+            $this->price = 0;
+        }
+        return $this;
+    }
+    public function getAvaliable()
+    {
+        return $this->avaliable;
+    }
+    private function setAvaliable()
+    {
+        if($this->price == 0){
+            $this->avaliable = false;
+        }else{
+            $this->avaliable = true;
+        }
         return $this;
     }
 }
